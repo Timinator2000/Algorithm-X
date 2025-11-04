391d1a4a711cf130a677668d0a76971d03061a02
Timinator2000 <v0t2ugh@gmail.com>
2025-11-03 19:23:31 -0700

    Deployed 20798ba with MkDocs version: 1.6.1

---
+++ javascripts/counter.js
@@ -0,0 +1,8 @@
+// Counter.dev loader
+(function() {
+  var s = document.createElement("script");
+  s.src = "https://cdn.counter.dev/script.js";
+  s.setAttribute("data-id", "1c8d14f0-def7-4fb8-a929-7a79713c8476"); // your Counter.dev ID
+  s.setAttribute("data-utcoffset", "-7"); // adjust your timezone offset if needed
+  document.body.appendChild(s);
+})();
