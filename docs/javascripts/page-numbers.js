73ac81e6d1ee29ef251091955feeefbef792daa2
Timinator2000 <v0t2ugh@gmail.com>
2025-11-10 20:51:24 -0700

    adjusted page counter down a bit

--- docs/javascripts/page-numbers.js
+++ docs/javascripts/page-numbers.js
@@ -25,7 +25,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
       Object.assign(counter.style, {
         position: "fixed",
-        top: "0.82rem",
+        top: "0.81rem",
         left: "50%",
         transform: "translateX(-50%)",
         background: "var(--md-default-bg-color)",
