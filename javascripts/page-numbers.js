e5ae578f155e8bb7870f876b75903f16815a7faf
Timinator2000 <v0t2ugh@gmail.com>
2025-11-10 20:48:55 -0700

    Deployed 8bfd6ec with MkDocs version: 1.6.1

--- javascripts/page-numbers.js
+++ javascripts/page-numbers.js
@@ -25,7 +25,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
       Object.assign(counter.style, {
         position: "fixed",
-        top: "0.8rem",
+        top: "0.85rem",
         left: "50%",
         transform: "translateX(-50%)",
         background: "var(--md-default-bg-color)",
